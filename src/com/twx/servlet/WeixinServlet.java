6bde07fff5898f705abdec11bb334cbf472356df
“唐文兴 <tangwenxing@nebulae.com.cn>
2016-07-11 22:59:53 +0800

    微信公众平台服务器接入

---
+++ src/com/twx/servlet/WeixinServlet.java
@@ -0,0 +1,31 @@
+package com.twx.servlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.twx.util.CheckUtil;
+
+public class WeixinServlet extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("1111111111!!!");
+		String signature = req.getParameter("signature");
+		String timestamp = req.getParameter("timestamp");
+		String nonce = req.getParameter("nonce");
+		String echostr = req.getParameter("echostr");
+		
+		PrintWriter out = resp.getWriter();
+		if(CheckUtil.checkSignature(signature, timestamp, nonce)){
+			out.print(echostr);
+		}
+	}
+}
---
+++ src/com/twx/util/CheckUtil.java
@@ -0,0 +1,64 @@
+package com.twx.util;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+/**
+ * 校验
+ * @author twx
+ *
+ */
+public class CheckUtil {
+	private static final String TOKEN ="tangwenxing";
+	
+	public static boolean checkSignature(String signature,String timestamp,String nonce){
+		String[] arr = new String[]{TOKEN,timestamp,nonce};
+		//排序
+		Arrays.sort(arr);
+		//生成字符串
+		StringBuffer sb = new StringBuffer();
+		for(int i=0;i<arr.length;i++){
+			sb.append(arr[i]);
+		}
+		
+		String temp = getSha1(sb.toString());
+		System.out.println(temp);
+		
+		return temp.equals(signature);
+	}
+	
+	/**
+	 * sha1加密
+	 * @param decript
+	 * @return
+	 */
+	public static String getSha1(String str){
+	    if (null == str || 0 == str.length()){
+	        return null;
+	    }
+	    char[] hexDigits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+	            'a', 'b', 'c', 'd', 'e', 'f'};
+	    try {
+	        MessageDigest mdTemp = MessageDigest.getInstance("SHA1");
+	        mdTemp.update(str.getBytes("UTF-8"));
+	         
+	        byte[] md = mdTemp.digest();
+	        int j = md.length;
+	        char[] buf = new char[j * 2];
+	        int k = 0;
+	        for (int i = 0; i < j; i++) {
+	            byte byte0 = md[i];
+	            buf[k++] = hexDigits[byte0 >>> 4 & 0xf];
+	            buf[k++] = hexDigits[byte0 & 0xf];
+	        }
+	        return new String(buf);
+	    } catch (NoSuchAlgorithmException e) {
+	        e.printStackTrace();
+	    } catch (UnsupportedEncodingException e) {
+	        e.printStackTrace();
+	    }
+		return "";
+	}
+}
